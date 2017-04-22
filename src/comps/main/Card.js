f7eacdf314828a1b2db37f3cef1dc6cba5e727ee
Michael Abdulai <michaelabdulai@MacBook-Air-van-Michael.local>
2017-04-22 14:59:01 +0200

    Card info complete

--- src/comps/main/Card.js
+++ src/comps/main/Card.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react'
 
 class Card extends Component {
   render () {
-    const { data, card } = this.props;
+    const { data, card: {title, subtitle, value} } = this.props;
     return (
       <div className="card">
-        <div className='card__title'>{card.title}</div>
+        <div className='card__title'>{title}</div>
+        <div className='card__subtitle'>{subtitle}</div>
+        <div className='card__value'>{value}</div>
+        
       </div>
     )
   }
--- src/comps/main/MainContent.js
+++ src/comps/main/MainContent.js
@@ -6,11 +6,23 @@ import Empty from './Empty';
 class MainContent extends Component {
 
   renderCards() {
-    const { data } = this.props;
+    const { data : { stats : competitive } } = this.props;
     const cards1 = [
-      {title: 'StreetFighter', value: 200 },
-      {title: 'StreetFighter', value: 200 },
-      {title: 'StreetFighter', value: 200 },
+      {title: 'StreetFighter', subtitle: 'melee final blows', value: competitive.game_stats.melee_final_blows },
+      {title: 'U are on fire', subtitle: 'time spent on fire', value: 200 },
+      {title: 'Stay on the point', subtitle: 'time spent on the point', value: 200 },
+    ];
+    
+    const cards2 = [
+      {title: 'Time Wasted', subtitle: 'total time played', value: 200 },
+      {title: 'Card collector', subtitle: 'total cards collected', value: 200 },
+      {title: 'Olympic gamer', subtitle: 'total medals collected', value: 200 },
+    ];
+
+    const cards3 = [
+      {title: 'Tie-maker', subtitle: 'total draw games', value: 200 },
+      {title: 'Multikill', subtitle: 'highest multikill', value: 200 },
+      {title: 'Greenpeace', subtitle: 'total enviromental kills', value: 200 },
     ];
 
     if(!data.stats) {
@@ -20,8 +32,8 @@ class MainContent extends Component {
     return (
       <div className="cardrow__container">
         <CardRow data={data} cards={cards1} />
-        <CardRow data={data} cards={cards1} />
-        <CardRow data={data} cards={cards1} />
+        <CardRow data={data} cards={cards2} />
+        <CardRow data={data} cards={cards3} />
       </div>
     );
   }
